b8c0704495eca762b9838a659e5b31383ccc9cd0
progmamun <mamun333089@gmail.com>
2022-05-29 23:48:29 +0600

    func-parameter-types-union-type-any

--- user-management/src/App.tsx
+++ user-management/src/App.tsx
@@ -3,6 +3,38 @@ import './App.css';
 import Counter from './components/Counter';
 
 function App() {
+  let student: string = 'Jeff';
+  let age: number = 69;
+  let isSmart: boolean = true;
+
+  let students: string[] = ['Joe', 'Biden', 'Harris'];
+  let fees: number[] = [12, 45, 78, 12, 45];
+
+  interface Person {
+    name: string;
+    job?: string;
+    employed: string | boolean;
+    age: number;
+    location?: string | number;
+  }
+
+  const person: Person = {
+    name: 'Bill Clinton',
+    employed: true,
+    age: 85,
+    location: 55,
+  };
+
+  const handleAddUser = (
+    firstName: string,
+    age: number,
+    address: string
+  ): number => {
+    const total: number = 50;
+    console.log(firstName, age, address);
+    return age;
+  };
+
   return <Counter></Counter>;
 }
 
